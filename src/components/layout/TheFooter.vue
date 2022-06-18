5b3123fd74e2c12c6341590c5c2a37bff8ad09e0
Aniket Kotal <44394628+mightykillrr@users.noreply.github.com>
2022-06-18 19:10:11 +0530

    added credits

--- src/components/layout/TheFooter.vue
+++ src/components/layout/TheFooter.vue
@@ -1,20 +1,14 @@
 <template>
   <div class="flex bg-dblue text-white items-center justify-between py-8 px-8">
     <h3 class="font-logo font-semibold text-4xl">stust</h3>
-    <!-- <ul class="flex gap-7 font-extralight">
-      <li class="hover:border-b-2 border-llgray">
-        <a href="#aboutus">About us</a>
-      </li>
-      <li class="hover:border-b-2 border-llgray">
-        <a href="#features">Features</a>
-      </li>
-      <li class="hover:border-b-2 border-llgray">
-        <a href="#pricing">Pricing</a>
-      </li>
-      <li class="hover:border-b-2 border-llgray">
-        <a href="#contactus">Contact us</a>
-      </li>
-    </ul> -->
+    <span class="text-lg font-light">
+      Made by
+      <a
+        href="https://github.com/mightykillrr"
+        class="text-primary underline font-bold"
+        >Aniket Kotal</a
+      >
+    </span>
 
     <span>&copy; 2022 stust.co</span>
   </div>
